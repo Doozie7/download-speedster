6c134aa4546c7ec38e9443b440e2f2add3a7830a
John Powell <john.powell@coats.com>
2023-09-21 01:05:13 +0100

    first commit

---
+++ readme.md
@@ -0,0 +1,53 @@
+# Network Speed Test
+
+A simple and effective tool to test the download speed of your internet connection. This project fetches a file and calculates the download speed in Mbps. Additionally, it fetches the IP address of the user and displays it.
+
+## Features
+
+- Fetches and displays the user's IP address.
+- Tests the internet download speed.
+- Displays the download speed in Mbps.
+- Visualizes the download speeds in a chart.
+- Calculates and displays the average download speed.
+
+## Setup
+
+1. **Clone the Repository**:
+
+   ```bash
+   git clone https://github.com/doozie7/network-speed-test.git
+   ```
+
+2. **Navigate to the Directory**:
+
+   ```bash
+   cd network-speed-test
+   ```
+
+3. **Open `index.html`**:
+
+   You can simply open `index.html` in your browser to see the tool in action.
+
+## Usage
+
+1. The tool will automatically start testing the download speed when you open the page.
+2. The tool will display the current download speed in Mbps.
+3. After multiple runs, the tool will display the average download speed on the chart.
+
+## Code Structure
+
+- `index.html`: Contains the main HTML structure of the tool.
+- `styles.css`: Contains all the styling for the page.
+- `speedtest.js`: Contains the main JavaScript functions responsible for testing the download speed, fetching the IP address, and drawing the chart.
+
+## Contributing
+
+1. Fork the repository.
+2. Create a new branch for your changes.
+3. Make your changes.
+4. Commit and push your changes.
+5. Create a pull request.
+
+## License
+
+This project is licensed under the MIT License.
---
+++ speedtest.js
@@ -0,0 +1,135 @@
+(function() {
+    const MAX_RUNS = 20;
+    const WAIT_DURATION = 1000;
+    const DOWNLOAD_SIZE = 1063179;
+    const DOWNLOAD_URL = "blob.data";
+    
+    let speedData = [];
+    let labelData = [];
+    let counter = 0;
+    
+    function updateElementText(id, text) {
+        const element = document.getElementById(id);
+        if (element) {
+            element.textContent = text;
+        }
+    }
+
+    function startDownloadTest() {
+        const startTime = Date.now();
+
+        fetch(DOWNLOAD_URL, { cache: 'no-store' })
+            .then(response => response.blob())
+            .then(blob => {
+                const endTime = Date.now();
+                const duration = (endTime - startTime) / 1000;
+                const bitsLoaded = DOWNLOAD_SIZE * 8;
+                const speedBps = bitsLoaded / duration;
+                const speedMbps = (speedBps / 1000000).toFixed(2);
+                
+                speedData.push(speedMbps);
+                labelData.push(counter.toString());
+                
+                updateElementText("downloadSpeed", speedMbps);
+            })
+            .catch(error => {
+                console.error("Error in download test:", error);
+                updateElementText("downloadSpeed", "Error");
+            });
+    }
+
+    function continuousTest() {
+        if (counter < MAX_RUNS) {
+            startDownloadTest();
+            let op = 100 - ((counter / MAX_RUNS) * 100);
+            document.getElementById("loadingPic").style="opacity: " + op.toString() + "%";
+            console.log(`Test run: ${counter}`);
+            counter++;
+            setTimeout(continuousTest, WAIT_DURATION);
+        } else {
+            document.getElementById("loadingPic").style="display: none";
+            drawChart();
+        }
+    }
+
+    function calculateAverage(data) {
+        const sum = data.reduce((a, b) => parseFloat(a) + parseFloat(b), 0);
+        return sum / data.length;
+    }
+
+    function drawChart() {
+        let averageSpeed = calculateAverage(speedData);
+
+        // Create a graph using Chart.js
+        let ctx = document.getElementById('speedChart').getContext('2d');
+        let speedChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: labelData, 
+                datasets: [{
+                    label: 'Speed (Mbps)',
+                    data: speedData,
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    borderWidth: 1,
+                    fill: true
+                }]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                },
+                plugins: {
+                    annotation: {
+                        annotations: {
+                            averageLine: {
+                                type: 'line',
+                                yMin: averageSpeed,
+                                yMax: averageSpeed,
+                                borderColor: 'rgb(26, 179, 148)',
+                                borderWidth: 2,
+                                label: {
+                                    enabled: true,
+                                    content: 'Average',
+                                    position: 'start'
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        });
+    
+        document.getElementById('speedChart').style = "display: block;";
+        document.getElementById("downloadSpeed").textContent = averageSpeed.toFixed(2);
+    }
+
+    function fetchIPAddress() {
+        fetch('https://api.ipify.org/?format=json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            document.getElementById("ipAddress").textContent = data.ip;
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error.message);
+            document.getElementById("ipAddress").textContent = 'Error fetching IP address';
+        });
+    }
+
+    fetchIPAddress();
+    continuousTest();
+})();
+
+
+
+
+
